dabf08fa3fde3ae5252076fad3ac1b8f8bd23431
Tom Kurian <35106657+tomkurian77@users.noreply.github.com>
2018-06-04 19:27:25 +0530

    Add files via upload

--- tcsspark.py
+++ tcsspark.py
@@ -1,11 +1,8 @@
+#Spark UseCase Solution
+
 from __future__ import print_function
-
 from os.path import expanduser, join, abspath
-
-from pyspark.sql import SparkSession
-
-from pyspark.sql import Row
-
+from pyspark.sql import SparkSession,Row
 from pyspark.sql.types import *
 from pyspark.sql.readwriter import DataFrameWriter
 
@@ -15,17 +12,20 @@ if __name__ == "__main__":
 
     spark.sql("create  table if not exists tcsques(videoid string,uploader string,erval int,category string,length int,view int,rating int,ratingnum int,comments int,related string) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TextFile")
 
-    spark.sql("create  table if not exists ques1(category string,count int)  STORED AS ParquetFile")
-    spark.sql("create  table if not exists ques2(videoid string,rating int)  STORED AS ParquetFile")
+    spark.sql("create  table if not exists test1(category string,num_videos int)  STORED AS ParquetFile")
+    spark.sql("create  table if not exists test2(videoid string,rating int)  STORED AS ParquetFile")
 
     spark.sql("LOAD DATA INPATH '/home/tom/youtubedata.txt' OVERWRITE INTO TABLE tcsques")
     df = spark.sql("SELECT * from tcsques")
-    df.write.mode('overwrite').format("parquet").save("hdfs://localhost/home/tom/yout")
+    df.write.mode('overwrite').format("parquet").save("hdfs://localhost/home/tom/sparkhive")
     a = df.select("category").groupBy("category").count().sort("count",ascending=False).limit(5)
     a.show()    
     b = df.select(("videoid"),("rating")).sort("rating",ascending=False).limit(10)
     b.show()
-    a.write.insertInto("ques1",overwrite = True)
-    b.write.insertInto("ques2",overwrite = True)
+    a.write.insertInto("test1",overwrite = True)
+    c = spark.sql("SELECT * from test1")
+    b.write.insertInto("test2",overwrite = True)
+    c.write.mode('overwrite').format("parquet").save("hdfs://localhost/home/tom/spark1")
+    b.write.mode('overwrite').format("parquet").save("hdfs://localhost/home/tom/spark2")
 
     spark.stop()
